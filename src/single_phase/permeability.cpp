22d0c5d23a994cf0e996fdbc70b2c184172937c0
javier <jesantos@utexas.edu>
2026-04-14 20:33:57 -0600

    removed the old logic of relPerm

--- src/single_phase/permeability.cpp
+++ src/single_phase/permeability.cpp
@@ -15,17 +15,6 @@
 using namespace plb;
 using namespace std;
 
-namespace patch
-{
-  template < typename T > std::string to_string( const T& n )
-  {
-    std::ostringstream stm ;
-    stm << n ;
-    return stm.str() ;
-  }
-}
-
-
 using namespace plb;
 
 typedef double T;
@@ -82,48 +71,28 @@ private:
 };
 
 // This function grabs the appropiate geometry for the single-phase simulation
-void readGeometry(std::string fNameIn, std::string fNameOut,
-  MultiScalarField3D<int>& geometry, plint run, plint runnum, bool vtk_out,
-  std::string GeometryName)
+void readGeometry(
+  const std::string& inputFolder,
+  MultiScalarField3D<int>& geometry,
+  bool vtk_out,
+  const std::string& geometryName)
 {
   const plint nx = geometry.getNx();
   const plint ny = geometry.getNy();
   const plint nz = geometry.getNz();
-  plint run_diff = ((runnum - 1)/2)+1;
-  std::string fNameIn_temp1 = fNameIn + GeometryName;
-  std::string fNameIn_temp = "0";
-
-  pcout  << "\n" << " Run: "<< run << std::endl;
+  const std::string geometryPath = inputFolder + geometryName + ".dat";
 
   Box3D sliceBox(0,0, 0,ny-1, 0,nz-1);
 
-  // Selects between the original geometry or the fluid 1,2 final config from multiphase sim
-
-  if (run == 1) {  // original geometry - absolute permeability
-    fNameIn_temp = fNameIn_temp1 + ".dat";
-    pcout  << "Running absolute permeability "<< std::endl;
-  }
-
-  if (run > run_diff) { // Fluid 1 : Krnw
-    const plint runner = run - run_diff;
-    fNameIn_temp =  fNameIn + "f1_for_kr_" + patch::to_string(runner)+".dat";
-    pcout  << "Running kr_f1 "<< std::endl;
-  }
-
-  if ( run > 1 && run < (run_diff+1) ) { // Fluid 2 : Krw
-    fNameIn_temp =  fNameIn + "f2_for_kr_"+ patch::to_string(run-1)+".dat";
-    pcout  << "Running kr_f2 "<< std::endl;
-  }
-
-
-  pcout  << "The geometry name is  "<< fNameIn_temp << std::endl;
+  pcout << "\nRunning absolute permeability " << std::endl;
+  pcout << "The geometry name is  " << geometryPath << std::endl;
 
   std::unique_ptr<plb::MultiScalarField3D<int> > slice = generateMultiScalarField<int>(geometry, sliceBox);
-  plb_ifstream geometryFile(fNameIn_temp.c_str());
+  plb_ifstream geometryFile(geometryPath.c_str());
 
   for (plint iX=0; iX<nx-1; ++iX) {
     if (!geometryFile.is_open()) {
-      pcout << "Error: could not open the geometry file " << fNameIn_temp << std::endl;
+      pcout << "Error: could not open the geometry file " << geometryPath << std::endl;
       exit(EXIT_FAILURE);
     }
 
@@ -132,7 +101,7 @@ void readGeometry(std::string fNameIn, std::string fNameOut,
   }
 
   if  (vtk_out == true) {
-    VtkImageOutput3D<T> vtkOut(createFileName("PorousMedium", run, 6), 1.0);
+    VtkImageOutput3D<T> vtkOut(createFileName("PorousMedium", 1, 6), 1.0);
     vtkOut.writeData<float>(*copyConvert<int,T>(geometry, geometry.getBoundingBox()), "tag", 1.0);
   }
 
@@ -244,16 +213,16 @@ void porousMediaSetup(MultiBlockLattice3D<T,DESCRIPTOR>& lattice,
     std::string fNameIn ;
     std::string fNameOut;
 
-    plint nx;
-    plint ny;
-    plint nz;
-    T deltaP ;
-    T Run;
-	  bool nx_p, ny_p, nz_p;
-	  bool vtk_out;
-    std::string GeometryName ;
-    plint maxT;
-	  T conv;
+	    plint nx;
+	    plint ny;
+	    plint nz;
+	    T deltaP ;
+	    plint requestedRuns;
+		  bool nx_p, ny_p, nz_p;
+		  bool vtk_out;
+	    std::string GeometryName ;
+	    plint maxT;
+		  T conv;
 
     std::string xmlFname;
     try {
@@ -279,14 +248,14 @@ void porousMediaSetup(MultiBlockLattice3D<T,DESCRIPTOR>& lattice,
       document["geometry"]["per"]["z"].read(nz_p);
 
 
-      document["folder"]["out_f"].read(fNameOut);
-      document["folder"]["in_f"].read(fNameIn);
+	      document["folder"]["out_f"].read(fNameOut);
+	      document["folder"]["in_f"].read(fNameIn);
 
-      document["simulations"]["press"].read(deltaP);
-      document["simulations"]["num"].read(Run);
-      document["simulations"]["iter"].read(maxT);
-	    document["simulations"]["conv"].read(conv);
-	    document["simulations"]["vtk_out"].read(vtk_out);
+	      document["simulations"]["press"].read(deltaP);
+	      document["simulations"]["num"].read(requestedRuns);
+	      document["simulations"]["iter"].read(maxT);
+		    document["simulations"]["conv"].read(conv);
+		    document["simulations"]["vtk_out"].read(vtk_out);
 
     }
     catch (PlbIOException& exception) {
@@ -296,20 +265,22 @@ void porousMediaSetup(MultiBlockLattice3D<T,DESCRIPTOR>& lattice,
     }
 
 
-    std::string inputF= fNameIn;
-    global::directories().setOutputDir(fNameOut+"/");
-    global::directories().setInputDir(inputF+"/");
+	    std::string inputF= fNameIn;
+	    global::directories().setOutputDir(fNameOut+"/");
+	    global::directories().setInputDir(inputF+"/");
 
+	    if (requestedRuns != 1) {
+	      pcout << "This single-phase solver supports exactly one simulation per input file. "
+	            << "Set <num> to 1." << std::endl;
+	      return -1;
+	    }
+
+	    const plint run = 1;
 	    const T omega = 1.0;
 	    const T nu    = ((T)1/omega- (T)0.5)/DESCRIPTOR<T>::invCs2;
-	    const plint runnum = Run;
-
-	    T perm[runnum];
-    T meanU[runnum];
-    T rel_perm[runnum];
-    T Perm;
-    T Vel;
-    pcout << "Total simulations: " << runnum << std::endl;
+	    T permeability = (T)0;
+	    T meanVelocity = (T)0;
+    pcout << "Total simulations: 1" << std::endl;
     pcout << "The convergence threshold is: " << conv << " %" << std::endl;
 
 
@@ -317,8 +288,6 @@ void porousMediaSetup(MultiBlockLattice3D<T,DESCRIPTOR>& lattice,
 	    if (!gifOutputEnabled) {
 	      pcout << "Skipping GIF previews because ImageMagick `convert` was not found." << std::endl;
 	    }
-
-	    for (plint run = 1; run <= runnum; ++run) {
 
       MultiBlockLattice3D<T,DESCRIPTOR> lattice( nx,ny,nz,
                                         new BGKdynamics<T,DESCRIPTOR>(omega) );
@@ -330,7 +299,7 @@ void porousMediaSetup(MultiBlockLattice3D<T,DESCRIPTOR>& lattice,
       lattice.periodicity().toggle(2, nz_p);
 
       MultiScalarField3D<int> geometry(nx,ny,nz);
-      readGeometry(fNameIn, fNameOut, geometry, run, runnum, vtk_out, GeometryName);
+      readGeometry(fNameIn, geometry, vtk_out, GeometryName);
 
       porousMediaSetup(lattice, createLocalBoundaryCondition3D<T,DESCRIPTOR>(),
                        geometry, deltaP);
@@ -356,7 +325,7 @@ void porousMediaSetup(MultiBlockLattice3D<T,DESCRIPTOR>& lattice,
           pcout << "Relative difference of Energy: " << setprecision(3)
           << relE_f1 <<" %"<<std::endl;
           pcout << "The preliminary permeability is: " <<std::endl;
-	      computePermeability(lattice, nu, deltaP, lattice.getBoundingBox(), Perm, Vel);
+	      computePermeability(lattice, nu, deltaP, lattice.getBoundingBox(), permeability, meanVelocity);
           pcout << "**********************************************" <<std::endl;
           if ( relE_f1<conv ){
             break;
@@ -365,10 +334,10 @@ void porousMediaSetup(MultiBlockLattice3D<T,DESCRIPTOR>& lattice,
         }
       }
 
-      pcout << "End of simulation at iteration " << iT << " for Run "<< run << std::endl;
+      pcout << "End of simulation at iteration " << iT << std::endl;
 
       //   pcout << "Permeability:" << std::endl;
-	      computePermeability(lattice, nu, deltaP, lattice.getBoundingBox(), Perm, Vel, false);
+	      computePermeability(lattice, nu, deltaP, lattice.getBoundingBox(), permeability, meanVelocity, false);
 
 		    if (gifOutputEnabled) {
 		      writeGifs(lattice,iT,run);
@@ -382,42 +351,17 @@ void porousMediaSetup(MultiBlockLattice3D<T,DESCRIPTOR>& lattice,
       plb_ofstream ofile4( rho_name.c_str() );
       ofile4 << setprecision(10) <<*computeDensity(lattice) << endl;
 
-      perm[run]=Perm;
-      meanU[run]=Vel;
-
-      rel_perm[run]=perm[run]/perm[1];
-	      if (run == 1) {
-	        pcout << "Absolute Permeability [l.u.^2] = " << perm[run] << std::endl;
-	      }
-	      if (runnum > 1) {
-	        pcout << "Relative Permeability = " << rel_perm[run] << std::endl;
-	      }
+      pcout << "Absolute Permeability [l.u.^2] = " << permeability << std::endl;
 
 	 if  (vtk_out == true) {
       pcout << "Writing VTK file ..." << std::endl;
       writeVTK(lattice, iT, run);
 	 }
-    }
 
 	    pcout << "Printing outputs" << std::endl;
-	    std::string outDir = fNameOut + "/";
 	    std::string output = outDir + "relPerm&vels.txt";
 	    plb_ofstream ofile(output.c_str());
 	    ofile << "Outputs" << "\n\n";
-	    for (plint runs = 1; runs <= runnum; ++runs) {
-	      if (runnum > 1) {
-	        ofile << "Run   = " << runs << std::endl;
-	      }
-	      if (runs == 1) {
-	        ofile << "Absolute Permeability [l.u.^2]   = " << perm[runs] << std::endl;
-	      }
-	      if (runnum > 1) {
-	        ofile << "Relative Permeability   = " << rel_perm[runs] << std::endl;
-	      }
-	      ofile << "Mean Velocity [l.u.]   = " << meanU[runs] << std::endl;
-	      if (runs < runnum) {
-	        ofile << std::endl;
-	      }
-
-	    }
+	    ofile << "Absolute Permeability [l.u.^2]   = " << permeability << std::endl;
+	    ofile << "Mean Velocity [l.u.]   = " << meanVelocity << std::endl;
 	  }
